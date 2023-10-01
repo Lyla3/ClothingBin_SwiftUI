463c62a4ebdab8e9d1115db0a193a3fde20e403d
Lyla3 <iiinin74@gmail.com>
2023-10-01 17:49:18 +0900

    [Fix] mapView에서 선택하지 않은 버튼 영역이 눌리는 현상 수정

--- ClothingBinSearch/View/MapView.swift
+++ ClothingBinSearch/View/MapView.swift
@@ -9,47 +9,55 @@ import SwiftUI
 import NMapsMap
 
 struct MapView: View {
-    //
-
-//    @ObservedObject var clothingBinStore: ClothingBinStore = ClothingBinStore()
-
+    
+    
+    //    @ObservedObject var clothingBinStore: ClothingBinStore = ClothingBinStore()
+    
     //@State private var coord: NMGLatLng = NMGLatLng(lat: 36.444, lng: 127.332)
+    @State var isShowingZoomlevelGuide: Bool = Coordinator.shared.isShowingZoomlevelGuide
+    
     var body: some View {
         ZStack(alignment:.topTrailing) {
             NaverMap().ignoresSafeArea()
-//            Button(action: {
-//                Coordinator.shared.clothingBinStore.handleButtonTap(buttonType: .currentLocation)
-////                clothingBinStore.handleButtonTap(buttonType: .currentLocation)
-//                        }) {
-//                            Text("현재위치버튼")
-//                        }
-            Button {
-                Coordinator.shared.clothingBinStore.handleButtonTap(buttonType: .currentLocation)
-            } label: {
-                Text("현재위치")
-                    .background(
-                        Rectangle()
-                            .fill(.white)
-                    )
-                    .padding()
+            HStack{
+                Button {
+                    Coordinator.shared.clothingBinStore.handleButtonTap(buttonType: .currentLocation)
+                } label: {
+                    Text("현재위치")
+                        .background(
+                            Rectangle()
+                                .fill(.white)
+                        )
+                        .padding()
+                }
             }
-            Button {
-                Coordinator.shared.clothingBinStore.handleButtonTap(buttonType: .currentMap)
-            } label: {
-                Text("현재 지도")
-                    .background(
-                        Rectangle()
-                            .fill(.white)
-                    )
-                    .padding()
-                    .position(x:350, y:80)
+            HStack{
+                Button {
+                    Coordinator.shared.clothingBinStore.handleButtonTap(buttonType: .currentMap)
+                } label: {
+                    Text("현재 지도")
+                        .background(
+                            Rectangle()
+                                .fill(.white)
+                        )
+                        .padding()
+                }
+                .position(x:350, y:80)
+               
             }
-
+            if isShowingZoomlevelGuide {
+                ZoomLevelGuideView()
+                    .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2 + 200)
+                    .onAppear {
+                        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { timer in
+                            isShowingZoomlevelGuide = false
+                        }
+                    }
+            }
         }
         .zIndex(1)
         .onAppear{
             Coordinator.shared.checkIfLocationServicesIsEnabled()
-            //Coordinator.shared.userDataStore.user = userDataStore.user
             Coordinator.shared.moveCameraPosition()
             Coordinator.shared.makeMarkers()
         }
