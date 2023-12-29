29883e672cf6d2cee895436f9dd2a77bd98c7566
Lyla3 <iiinin74@gmail.com>
2023-12-30 01:00:47 +0900

    [Add] Report View 신고 뷰 제작

---
+++ ClothingBinSearch/View/ReportView.swift
@@ -0,0 +1,18 @@
+//
+//  ReportView.swift
+//  ClothingBinSearch
+//
+//  Created by Lyla on 2023/12/30.
+//
+
+import SwiftUI
+
+struct ReportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReportView()
+}
