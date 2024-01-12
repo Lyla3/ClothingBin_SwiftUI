6c9245fa93207a7767c157abe53072bc1e1d3835
Lyla3 <iiinin74@gmail.com>
2024-01-13 00:15:10 +0900

    [Add] 서초구, 노원구 의류수거함 데이터 추가

--- ClothingBinSearch/Model/Region.swift
+++ ClothingBinSearch/Model/Region.swift
@@ -10,9 +10,11 @@ import Foundation
 enum Region: String, CaseIterable {
     case Gangnam = "서울시 강남구"
     case Gangdong = "서울시 강동구"
+    case Nowon = "서울시 노원구"
     case Dongjak = "서울시 동작구"
     case Guro = "서울시 구로구"
     case Mapo = "서울시 마포구"
+    case Seocho = "서울시 서초구"
     case Yangcheon = "서울시 양천구"
     case YeoungDeugpo = "서울시 영등포구"
     case Gwanak = "서울시 관악구"
@@ -26,12 +28,16 @@ enum Region: String, CaseIterable {
             return "Seoul_Gangnam"
         case .Gangdong :
             return "Seoul_Gangdong"
+        case .Nowon :
+            return "Seoul_Nowon"
         case .Dongjak :
             return "Seoul_Dongjak"
         case .Guro :
             return "Seoul_guro"
         case .Mapo :
             return "Seoul_Mapo"
+        case .Seocho :
+            return "Seoul_Seocho"
         case .Yangcheon :
             return "Seoul_Yangcheon"
         case .YeoungDeugpo :
