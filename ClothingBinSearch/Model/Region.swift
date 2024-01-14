de179bec58eb2e59d3fd9b232e6a8cc947e11155
Lyla3 <iiinin74@gmail.com>
2024-01-15 00:10:32 +0900

    [Add] ClothingbinData
    - 서초구, 노원구, 송파구, 금천구, 광진구, 동대문구, 은평구 의류수거함 데이터 추가

--- ClothingBinSearch/Model/Region.swift
+++ ClothingBinSearch/Model/Region.swift
@@ -10,43 +10,58 @@ import Foundation
 enum Region: String, CaseIterable {
     case Gangnam = "서울시 강남구"
     case Gangdong = "서울시 강동구"
+    case Gwanak = "서울시 관악구"
+    case Gwangjin = "서울시 광진구"
+    case Geumcheon = "서울시 금천구"
     case Nowon = "서울시 노원구"
+    case Dongdaemun = "서울시 동대문구"
     case Dongjak = "서울시 동작구"
     case Guro = "서울시 구로구"
     case Mapo = "서울시 마포구"
     case Seocho = "서울시 서초구"
+    case Songpa = "서울시 송파구"
     case Yangcheon = "서울시 양천구"
     case YeoungDeugpo = "서울시 영등포구"
-    case Gwanak = "서울시 관악구"
+    case Eunpyeong = "서울시 은평구"
     case Seodaemun = "서울시 서대문구"
     case Gongro = "서울시 종로구"
     
     
     func getFileName() -> String {
         switch self {
-        case .Gangnam :
+        case .Gangnam:
             return "Seoul_Gangnam"
-        case .Gangdong :
+        case .Gangdong:
             return "Seoul_Gangdong"
-        case .Nowon :
+        case .Geumcheon:
+            return "Seoul_Geumcheon"
+        case .Gwangjin:
+            return "Seoul_Gwangjin"
+        case .Nowon:
             return "Seoul_Nowon"
-        case .Dongjak :
+        case .Dongdaemun:
+            return "Seoul_Dongdaemun"
+        case .Dongjak:
             return "Seoul_Dongjak"
-        case .Guro :
+        case .Guro:
             return "Seoul_guro"
-        case .Mapo :
+        case .Mapo:
             return "Seoul_Mapo"
-        case .Seocho :
+        case .Seocho:
             return "Seoul_Seocho"
-        case .Yangcheon :
+        case .Songpa:
+            return "Seoul_Songpa"
+        case .Yangcheon:
             return "Seoul_Yangcheon"
-        case .YeoungDeugpo :
+        case .YeoungDeugpo:
             return "Seoul_Yeoungdeungpo"
-        case .Gwanak :
+        case .Eunpyeong:
+            return "Seoul_Eunpyeong"
+        case .Gwanak:
             return "Seoul_gwanak"
-        case .Seodaemun :
+        case .Seodaemun:
             return "Seoul_Seodaemun"
-        case .Gongro :
+        case .Gongro:
             return "Seoul_Gongro"
         }
     }
