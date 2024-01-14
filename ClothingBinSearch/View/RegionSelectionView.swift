c78f80ac1b568c5110ca56154bea7e0173542305
Lyla3 <iiinin74@gmail.com>
2024-01-15 00:46:19 +0900

    [Fix] RegionSelectionView에서 안내 문구 추가
    - 데이터 추가로 인해 첫 페이지에서 지역구가 다 보이지 않음.  스크롤 안내 문구 추가

--- ClothingBinSearch/View/RegionSelectionView.swift
+++ ClothingBinSearch/View/RegionSelectionView.swift
@@ -18,18 +18,25 @@ struct RegionSelectionView: View {
             ZStack{
                 HStack {
                     Spacer()
-                    Button(action: { isShowingModal = false }) {
-                        Image(systemName: "xmark")
-                            .foregroundColor(.black)
-                            .font(.title2)
-                    }
+                        Button(action: { isShowingModal = false }) {
+                            Image(systemName: "xmark")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                        }
+                        .frame(maxHeight: 50, alignment: .top)
                 }
                     HStack {
-                        Text("지역선택")
-                            .font(.title2)
+                        VStack(spacing: 5){
+                            Text("의류수거함 검색 지역 선택")
+                                .font(.title3)
+                            Text("더 많은 지역 정보는 스크롤하여 주시기 바랍니다.")
+                                .font(.footnote)
+                                .foregroundStyle(.gray)
+                        }
                     }
             }
-            .padding()
+            .padding(.top, 15)
+            .padding(.horizontal, 10)
             ScrollView{
                 ForEach(Region.allCases, id: \.self) { region in
                     Divider()
